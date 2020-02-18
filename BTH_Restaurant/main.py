3ca9c629367dd8e95e625b7d775fe3832adb2877
Kumuditha Athukorala <55691339+Kumuditha-Athukorala@users.noreply.github.com>
2020-02-18 14:43:31 +0000

    Basic Layout
    Upadated Basic  Layout

--- BTH_Restaurant/main.py
+++ BTH_Restaurant/main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/slider')
-def indexslider():
-    return render_template('imageslider.html')
+@app.route('/login')
+def indexLogin():
+    return render_template('login.html')
 
 if __name__ == '__main__':
 	app.run()
