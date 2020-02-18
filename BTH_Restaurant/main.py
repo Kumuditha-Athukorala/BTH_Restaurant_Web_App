e32cedbc5c0b4726fe3f5268ce59a0d13782450c
Kumuditha Athukorala <55691339+Kumuditha-Athukorala@users.noreply.github.com>
2020-02-18 11:25:44 +0000

    Flask Template
    Updated the existing template

--- BTH_Restaurant/main.py
+++ BTH_Restaurant/main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/slider')
+def indexslider():
+    return render_template('imageslider.html')
 
 if __name__ == '__main__':
 	app.run()
